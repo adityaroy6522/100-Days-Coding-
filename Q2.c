784d0991b373bc3450b3570b78c53d74de6030d1
adityaroy6522 <adityaroy6522@gmail.com>
2025-08-25 13:43:42 +0530

    arithmetical operations

---
+++ Q2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main ()
+{
+    float a,b,c,d,e,f;
+    printf("num1=");
+    scanf ("%f",&a);
+    printf ("num2=");
+    scanf ("%f",&b);
+    c=a+b;
+    printf ("sum of two number is %f\n",c);
+    d=a-b;
+    printf ("difference of two number is %f\n",d);
+    e=a*b;
+    printf ("product of two number is %f\n",e);
+    f=a/b;
+    printf ("division of two number is %f",f);
+
+    return 0;
+
+}
