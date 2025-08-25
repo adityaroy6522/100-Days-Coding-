348c11d79a772dd1c8c9c4bbec422895ff451df4
adityaroy6522 <adityaroy6522@gmail.com>
2025-08-25 11:48:50 +0530

    sum of two number

---
+++ Q1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main ()
+{
+    float a,b,c;
+    printf("a=");
+    scanf ("%f",&a);
+    printf("b=");
+    scanf ("%f",&b);
+    c=a+b;
+    printf ("%f",c);
+
+
+
+    return 0;
+
+
+
+
+
+
+
+
+
+}
