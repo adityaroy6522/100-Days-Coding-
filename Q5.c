a2cdcbb6404997921f2b2c860061a9d864ca5be2
adityaroy6522 <adityaroy6522@gmail.com>
2025-08-25 14:09:44 +0530

    Celcius to Fahrenheit

---
+++ Q5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+    float F,C;
+    printf ("temperature in celcius=");
+    scanf ("%f",&C);
+
+    F=(C*1.8)+32;
+    printf ("temperature in fahrenheit is %f",F);
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+}
