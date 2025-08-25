897ca0a05ee9639de064f93d870660314bb5581a
adityaroy6522 <adityaroy6522@gmail.com>
2025-08-25 17:45:40 +0530

    integer Swapping

---
+++ Q6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    int a,b;
+    printf ("please enter two integers:");
+    scanf ("%d %d",&a,&b);
+
+    printf ("integer after swapping %d %d",b,a);
+
+return 0;
+
+}
